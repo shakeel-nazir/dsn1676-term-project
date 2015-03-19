9db4cce0ba03d1a8b56c8b88e477f1394b423ed5
shakeel-nazir <shakeel.nazir1@gmail.com>
2015-03-18 22:22:32 -0400

    Add Cowboy
    - Add Cowboy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var $btn = $('.btn');
 var $panel = $('.panel');
 var $btncollapse = $('.btn-collapse-expand');
+var $btncowboy = $('.btn-cowboy');
 var $panel = $('.panel')
+var $cowboy1 = $('.cowboy1')
+var $cowboy = $('.cowboy')
 var $onmouse = $('.onmouse')
 
 var $billboard = $('.billboard');
@@ -16,5 +19,8 @@ $billboard.on('mousemove', function (e) {
     $rb.css('left', e.pageX);
 });
 
+$btncowboy.on('click', function () {
+    $cowboy.toggleClass('cowboy1');
+})
 
 
